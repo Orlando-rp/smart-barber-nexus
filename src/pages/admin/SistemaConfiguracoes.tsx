605ba30b6483eb251b372502f9a2f1577a1262de
Orlando Romulo Pinto <orlandoromulo@gmail.com>
2025-07-03 17:56:34 -0300

    Update SistemaConfiguracoes.tsx

--- src/pages/admin/SistemaConfiguracoes.tsx
+++ src/pages/admin/SistemaConfiguracoes.tsx
@@ -1,3 +1,5 @@
-// src/pages/admin/SistemaConfiguracoes.tsx
-const ClientesSaas = () => <div>Configurações</div>
-export default SistemaConfiguracoes
+const SistemaConfiguracoes = () => {
+  return <div>Configurações do Sistema</div>;
+};
+
+export default SistemaConfiguracoes;
